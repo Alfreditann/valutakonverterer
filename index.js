5de9f6f1d943cb27b71d30d6da69552657e8d391
Alfreditann <alfred.ulsberg@gmail.com>
2026-02-26 12:11:51 +0100

    valuta converter middleware

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const { valutaConvert} = require("./middleware/add-remove")
+const { fetchData } = require("./middleware/fetch");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.set("view-engine", "ejs")
-
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
+    res.render("index");
+});
 
-app.post ("/", (req,res) => {
-    console.log("renderd /")
-})
+app.post("/", fetchData, (req, res) => {
+
+    const amount1 = req.body.amount1;
+    const base = req.body.currency1;
+    const target = req.body.currency2;
+    const rate = data.conversion_rates[target];
+
+
+    // const { amount1 } = req.body;
+    // const { base, target, rate } = req.exchangeData;
+
+    const convertedAmount = amount1 * rate;
+    console.log(amount1, rate)
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000"
-
-    )
-})
+    console.log("Server is running on http://localhost:3000");
+});
---
+++ middleware/fetch.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+
+async function fetchData(req, res, next) {
+    try {
+        const base = req.body.currency1;   
+        const target = req.body.currency2;
+
+        if (!base || !target) {
+            return res.status(400).json({ error: "Base and target required" });
+        }
+
+        const response = await fetch(
+            `https://v6.exchangerate-api.com/v6/310599a91976670c2383247f/latest/${base}`
+        );
+
+        if (!response.ok) {
+            return res.status(500).json({ error: "API request failed" });
+
+        }
+
+        const data = await response.json();
+
+        const rate = data.conversion_rates[target];
+
+        req.exchangeRate = {
+            base,
+            target,
+            rate
+        };
+
+        next();
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+module.exports = { fetchData }
